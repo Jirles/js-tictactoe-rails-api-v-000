b7c3d7a18690289402f7daa843a991d9bc6974e9
Emily Jirles <emily.jirles@gmail.com>
2018-07-23 15:55:39 +0000

    automatically backed up by learn

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -75,7 +75,7 @@ function attachListeners(){
   //clear button event
   $('#clear').click(function(){
     $('td').html('');
-    $('table').data(''); //remove gameid so things are saved over things
+    $('table').removeData(); //remove gameid so things are saved over things
   });
 };
 
